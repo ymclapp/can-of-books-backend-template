80ea8d5de2c73c2843edfc0705a13a7879f9f935
JB-Tellez <jb.tellez@gmail.com>
2021-08-29 09:11:12 -0700

    removes auth from main branch

--- server.js
+++ server.js
@@ -3,8 +3,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
 
 const app = express();
 app.use(cors());
@@ -13,11 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.get('/test', (request, response) => {
 
-  // TODO: 
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+  response.send('test request received')
 
 })
 
